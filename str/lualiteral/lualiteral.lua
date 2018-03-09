0acd4c7d221e41cdd42178344098a3a1084ab2f2
pocomane <pocomane@nomail.com>
2018-03-09 13:20:57 +0100

    Dump values as strings containing lua literals

---
+++ str/lualiteral/lualiteral.lua
@@ -0,0 +1,53 @@
+--ZFUNC-lualiteral-v1
+local function lualiteral( tab, alias ) --> result
+
+  -- export ???
+  local function atomdump( val )
+    local t = type( val )
+    if 'nil' == t then
+      return 'nil'
+    elseif 'number' == t then
+      return tostring( val )
+    elseif 'boolean' == t then
+      return tostring( val )
+    elseif 'string' ~= t then
+      val = tostring( val )
+    end
+    return string.format( "%q", val )
+  end
+
+  local function lualiteral_rec( cur, alias, result )
+
+    if 'table' ~= type( cur ) then
+      table.insert( result, atomdump( cur ))
+    else
+
+      -- If a table alias is defined, use that name
+      local substitute
+      if alias then substitute = alias[cur] end
+      if substitute then
+        table.insert( result, substitute )
+      else
+
+        -- Recurse over each key and each value
+        table.insert(result, '{')
+        for k, v in pairs( cur ) do
+          table.insert( result, '[' )
+          lualiteral_rec( k, alias, result )
+          table.insert( result, ']=' )
+          lualiteral_rec( v, alias, result )
+          table.insert( result, ',' )
+        end
+        table.insert( result, '}' )
+      end
+    end
+
+    return result
+  end
+
+  local result = lualiteral_rec( tab, alias, {} )
+  return table.concat( result )
+end
+
+return lualiteral
+
