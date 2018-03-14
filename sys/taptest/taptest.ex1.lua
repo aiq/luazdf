45cb29859a4180b26e5a604404a219b10e29ca9f
Alexander Schulz <aiqpost@yahoo.com>
2018-03-14 19:02:29 +0100

    Modified taptest function

--- sys/taptest/taptest.ex1.lua
+++ sys/taptest/taptest.ex1.lua
@@ -1,85 +1,66 @@
-
--- taptest is both the "Unit under test" (uut) and the "Test framework" (tf)
-local tf = require( "taptest" )
-local uut = require( "taptest" )
+local t = require "taptest"
+local taptest = require "taptest"
+-- taptest is both the "Unit under test" (taptest) and the "Test framework" (t)
 
 -- To avoid confusion (as much as it is possible) tf will be always used in its
 -- easest form: it just checks that the two argument are equals.
 -- Since taptest always returns what it print on stdout, the returned
--- value of uut is checked
+-- value of taptest is checked
 
--- wrap the uut to avoid printing test results on the stdout
+-- wrap the taptest to avoid printing test results on the stdout
 local fake_test_count = 1
-local uut_wrapped = uut
-local function uut(...)
-  if 1< select('#', ...) then print('ok '..fake_test_count) end
-  fake_test_count = fake_test_count + 2
-  local _p = print
-  print = function() end
-  local result = uut_wrapped(...)
-  print = _p
-  return result 
+local taptest_wrapped = taptest
+local function taptest( ... )
+   if 1 < select( "#", ... ) then print( "ok "..fake_test_count ) end
+   fake_test_count = fake_test_count + 2
+   local _p = print
+   print = function() end
+   local result = taptest_wrapped( ... )
+   print = _p
+   return result 
 end
 
-tf(
-  uut( 1, 1 ),
-  'ok 1' )
+t( taptest( 1, 1 ), "ok 1" )
 
--- Note: since at each line two test will be done (one for uut and one for tf)
+-- Note: since at each line two test will be done (one for taptest and one for tf)
 -- the test counter step is 2, not 1
-tf(
-  uut( 1, 1 ),
-  'ok 3' )
+t( taptest( 1, 1 ), "ok 3" )
 
 -- Additional infos when the test fails
-tf(
-  uut( 1, 2 ),
-  'not ok 5 - taptest.ex1.lua:19. Expectation [2] does not match with [1]. ' )
+t( taptest( 1, 2 ),
+   "not ok 5 - taptest.ex1.lua:18. Expectation [2] does not match with [1]. " )
 
 -- Custom infos on fail
-tf(
-  uut( 1, 2, 'Not good!' ),
-  'not ok 7 - taptest.ex1.lua:19. Expectation [2] does not match with [1]. Not good!' )
+t( taptest( 1, 2, "Not good!" ),
+   "not ok 7 - taptest.ex1.lua:18. Expectation [2] does not match with [1]. Not good!" )
 
 -- Custom compare function
-tf(
-  uut( 1, 2, function(a,b) return a < b end ),
-  'ok 9' )
-tf(
-  uut( 2, 1, function(a,b) return a < b end ),
-  'not ok 11 - taptest.ex1.lua:19. Expectation [1] does not match with [2]. ' )
+t( taptest( 1, 2, function( a, b ) return a < b end ),
+   "ok 9" )
+t( taptest( 2, 1, function( a, b ) return a < b end ),
+   "not ok 11 - taptest.ex1.lua:18. Expectation [1] does not match with [2]. " )
 
 -- Custom compare function and message
-tf(
-  uut( 2, 1, function(a,b) return a < b end, 'Not good!' ),
-  'not ok 13 - taptest.ex1.lua:19. Expectation [1] does not match with [2]. Not good!' )
-tf(
-  uut( 2, 1, 'Not good!', function(a,b) return a < b end ),
-  'not ok 15 - taptest.ex1.lua:19. Expectation [1] does not match with [2]. Not good!' )
+t( taptest( 2, 1, function( a, b ) return a < b end, "Not good!" ),
+   "not ok 13 - taptest.ex1.lua:18. Expectation [1] does not match with [2]. Not good!" )
+t( taptest( 2, 1, "Not good!", function( a, b ) return a < b end ),
+   "not ok 15 - taptest.ex1.lua:18. Expectation [1] does not match with [2]. Not good!" )
 
 -- Single argument = Tap diagnostic
-tf(
-  uut( 'new\nsuite' ),
-  '#\n#########\n# new\n# suite' )
+t( taptest( "new\nsuite" ), "#\n#########\n# new\n# suite" )
 
 -- Checker function can add useful information
+t( taptest( 1, 1, function( a, b ) return a == b, "- additional info" end ),
+   "ok 18 - additional info" )
 
-tf(
-  uut( 1, 1, function(a, b) return a==b, '- additional info' end ),
-  'ok 18 - additional info' )
-
-tf(
-  uut( 1, 2, function(a, b) return a==b, '- additional info' end ),
-  'not ok 20 - taptest.ex1.lua:19. Expectation [2] does not match with [1].  - additional info' )
+t( taptest( 1, 2, function( a, b ) return a == b, "- additional info" end ),
+   "not ok 20 - taptest.ex1.lua:18. Expectation [2] does not match with [1].  - additional info" )
 
 -- No argument = Summary and final plan
-tf(
-  uut( ),
-  '#\n#########\n# 6 tests failed\n1..21' )
+t( taptest(), "#\n#########\n# 6 tests failed\n1..21" )
 
-tf()
+t()
 
 -- In case all the tests are successful, the line
 -- # all is right
 -- will be substitued to the '# 5 tests failed' one
-
--- sys/taptest/taptest.lua
+++ sys/taptest/taptest.lua
@@ -1,78 +1,77 @@
---ZFUNC-taptest-v1
-
 local test_count = 0
 local fail_count = 0
 
-local function taptest(...) --> msg
+--ZFUNC-taptest-v0
+local function taptest( ... ) --> msg
 
-  local function diagnostic(desc)
-    local msg = '#\n#########\n# ' .. desc:gsub('\n','\n# ')
-    print(msg)
-    return msg
-  end
+   local function diagnostic( desc )
+      local msg = "#\n#########\n# "..desc:gsub( "\n", "\n# " )
+      print( msg )
+      return msg
+   end
 
-  local function print_summary()
-    local msg = ''
-    if fail_count == 0 then
-      msg = msg .. diagnostic('all is right')
-    else
-      msg = msg .. diagnostic(fail_count.. ' tests failed')
-    end
-    local plan = '1..'..test_count
-    print(plan)
-    return msg..'\n'..plan
-  end
+   local function print_summary()
+      local msg = ""
+      if fail_count == 0 then
+         msg = msg..diagnostic( "all is right" )
+      else
+         msg = msg..diagnostic( fail_count.." tests failed" )
+      end
+      local plan = "1.."..test_count
+      print( plan )
+      return msg.."\n"..plan
+   end
 
-  local function do_check(got, expected, a, b)
+   local function do_check(got, expected, a, b)
 
-    -- Extra arg parse and defaults
-    local checker, err
-    if 'string' == type(a) then err = a end
-    if 'string' == type(b) then err = b end
-    if not err then err = '' end
-    if 'function' == type(a) then checker = a end
-    if 'function' == type(b) then checker = b end
-    if not checker then checker = function(e, g) return e == g end end
+      -- Extra arg parse and defaults
+      local checker, err
+      if "string" == type(a) then err = a end
+      if "string" == type(b) then err = b end
+      if not err then err = "" end
+      if "function" == type(a) then checker = a end
+      if "function" == type(b) then checker = b end
+      if not checker then checker = function( e, g ) return e == g end end
 
-    -- Check the condition
-    test_count = test_count + 1
-    local ok, info = checker(got, expected)
+      -- Check the condition
+      test_count = test_count + 1
+      local ok, info = checker( got, expected )
 
-    -- Generate TAP line
-    local msg = ''
-    if ok then
-      msg = msg .. 'ok ' .. test_count
-    else
-      fail_count = fail_count + 1
+      -- Generate TAP line
+      local msg = ""
+      if ok then
+         msg = msg.."ok "..test_count
+      else
+         fail_count = fail_count + 1
 
-      -- Find position in source
-      local stackup = 2
-      local i = debug.getinfo(stackup)
-      while i.source == '=(tail call)' do
-        stackup = stackup + 1
-        i = debug.getinfo(stackup)
+         -- Find position in source
+         local stackup = 2
+         local i = debug.getinfo(stackup)
+         while i.source == "=(tail call)" do
+            stackup = stackup + 1
+            i = debug.getinfo(stackup)
+         end
+
+         msg = msg
+               .."not ok " .. test_count .. " - "
+               ..i.source:match( "([^@/\\]*)$" )..":"..i.currentline..". "
+               .."Expectation ["..tostring( expected ).."] "
+               .."does not match with ["..tostring( got ).."]. "
+               ..err
       end
 
-      msg = msg
-        .. 'not ok ' .. test_count .. ' - '
-        .. i.source:match('([^@/\\]*)$') .. ':' .. i.currentline .. '. '
-        .. 'Expectation [' .. tostring(expected) .. '] '
-        .. 'does not match with [' .. tostring(got) ..']. '
-        .. err
-    end
+      if info then msg = msg.." "..info end
+      print(msg)
+      return msg
+   end
 
-    if info then msg = msg .. ' ' .. info end
-    print(msg)
-    return msg
-  end
+   local narg = select( "#", ... )
+   if     0 == narg then return print_summary()
+   elseif 1 == narg then return diagnostic( select( 1, ... ) )
+   elseif 4 >= narg then return do_check( ... )
+   end
 
-  local narg = select('#', ...)
-  if     0 == narg then return print_summary()
-  elseif 1 == narg then return diagnostic(select(1, ...))
-  elseif 4 >= narg then return do_check(...)
-  end
-  return nil, 'Too many arguments'
+   return nil, "Too many arguments"
 end
 
 return taptest
-
