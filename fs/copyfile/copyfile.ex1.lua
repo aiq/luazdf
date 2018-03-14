8c6636158bf0d9758bae8ad7d3269904f639da6c
Alexander Schulz <aiqpost@yahoo.com>
2018-03-14 20:41:47 +0100

    modified the copyfile function

--- fs/copyfile/copyfile.ex1.lua
+++ fs/copyfile/copyfile.ex1.lua
@@ -1,19 +1,17 @@
+local t = require "taptest"
+local copyfile = require "copyfile"
+local readfile = require "readfile"
+local writefile = require "writefile"
 
-local copyfile = require 'copyfile'
-local r = require 'readfile'
-local w = require 'writefile'
-local t = require 'taptest'
+local inpath = "intmp.txt"
+local outpath = "outtmp.txt"
+local data = ( "01f" ):rep( 512 )
 
-local inpath = 'intmp.txt'
-local outpath = 'outtmp.txt'
+writefile( inpath, data )
+t( copyfile( inpath, outpath ), true )
+t( readfile( outpath ), data )
+
 os.remove( inpath )
 os.remove( outpath )
 
-local data = ('01f'):rep(512)
-
-w( inpath, data )
-t( copyfile( inpath, outpath ), true )
-t( r( outpath ), data )
-
 t()
-
