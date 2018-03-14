7447978edf9588fa3d537156733cd6ab75804a4e
Alexander Schulz <aiqpost@yahoo.com>
2018-03-14 20:56:12 +0100

    Initial modifications for appendfile

--- fs/appendfile/appendfile.ex1.lua
+++ fs/appendfile/appendfile.ex1.lua
@@ -1,20 +1,19 @@
+local t = require "taptest"
+local appendfile = require "appendfile"
+local readfile = require "readfile"
 
-local appendfile = require 'appendfile'
-local r = require 'readfile'
-local t = require 'taptest'
+t( appendfile( "appendfile.txt", "123" ), true )
+t( readfile( "appendfile.txt" ), "123" )
 
-os.remove( 'apptmp.txt' )
+t( appendfile( "appendfile.txt", "456" ), true )
+t( readfile( "appendfile.txt" ), "123456" )
 
-t( appendfile( 'apptmp.txt', '123' ), true )
-t( r( 'apptmp.txt' ),                 '123' )
-t( appendfile( 'apptmp.txt', '456' ), true )
-t( r( 'apptmp.txt' ),                 '123456' )
+t( appendfile( "appendfile.txt", { "7","8" } ), true )
+t( readfile( "appendfile.txt" ), "12345678" )
 
-t( appendfile( 'apptmp.txt', {'7','8'} ), true )
-t( r( 'apptmp.txt' ),                     '12345678' )
+t( appendfile( "appendfile.txt", {"9","10"}, "<", ">" ), true )
+t( readfile( "appendfile.txt" ), "12345678<9><10>" )
 
-t( appendfile( 'apptmp.txt', {'9','10'}, '<', '>' ), true )
-t( r( 'apptmp.txt' ),                     '12345678<9><10>' )
+os.remove( "appendfile.txt" )
 
 t()
-
--- fs/appendfile/appendfile.lua
+++ fs/appendfile/appendfile.lua
@@ -1,45 +1,45 @@
---ZFUNC-appendfile-v1
-local function appendfile( path, data, prefix, suffix ) --> ok, err
+--ZFUNC-appendfile-v0
+local function appendfile( path, data, prefix, suffix ) --> res, err
 
-  local function writeorclose( f, data )
-    local ok, err = f:write( data )
-    if err then f:close() end
-    return ok, err
-  end
+   local function writeorclose( f, data )
+      local res, err = f:write( data )
+      if err then f:close() end
+      return res, err
+   end
 
-  local d, derr = io.open( path, 'a+b' )
-  if derr then
-    return nil, 'Can not create or open destination file. ' .. derr
-  end
+   local d, derr = io.open( path, "a+b" )
+   if derr then
+      return nil, "Can not create or open destination file. "..derr
+   end
 
-  local ok, err = d:seek( 'end' )
-  if err then
-    d:close()
-    return nil, err
-  end
+   local ok, err = d:seek( "end" )
+   if err then
+      d:close()
+      return nil, err
+   end
 
-  if 'string' == type(data) then
-    data = { data }
-  end
+   if "string" == type( data ) then
+      data = { data }
+   end
 
-  -- Output loop
-  for i = 1, #data do
+   -- Output loop
+   for i = 1, #data do
 
-    if prefix then
-      ok, err = writeorclose( d, prefix )
+      if prefix then
+         ok, err = writeorclose( d, prefix )
+         if err then return ok, err end
+      end
+
+      ok, err = writeorclose( d, data[ i ] )
       if err then return ok, err end
-    end
 
-    ok, err = writeorclose( d, data[i] )
-    if err then return ok, err end
+      if suffix then
+         ok, err = writeorclose( d, suffix )
+         if err then return ok, err end
+      end
+   end
 
-    if suffix then
-      ok, err = writeorclose( d, suffix )
-      if err then return ok, err end
-    end
-  end
-
-  return d:close()
+   return d:close()
 end
 
 return appendfile
