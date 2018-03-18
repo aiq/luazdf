6ff968dfd4b1b140ce276ca4d5055e2b602b5a9b
pocomane <pocomane@nomail.com>
2018-03-18 17:38:13 +0100

    Windows support in escapeshellarg. Tests improvement.

--- cli/escapeshellarg/escapeshellarg.lua
+++ cli/escapeshellarg/escapeshellarg.lua
@@ -1,6 +1,33 @@
+
+local quote_function
+
 --ZFUNC-escapeshellarg-v1
 local function escapeshellarg( str ) --> esc
-   return '"'..str:gsub( '"', '\\"' )..'"'
+
+  local function posix_quote_argument(str)
+    str = str:gsub( "[$`\"\\]", "\\%1" )
+    return '"' .. str .. '"'
+  end
+
+  local function windows_quote_argument(str)
+    str = str:gsub('[%%&\\^<>|]', '^%1')
+    str = str:gsub('"', "\\%1")
+    str = str:gsub('[ \t][ \t]*', '"%1"')
+    return str
+  end
+
+  if not quote_function then
+    quote_function = windows_quote_argument
+    local shell = os.getenv('SHELL')
+    if shell then
+      if '/' == shell:sub(1,1) and 'sh' == shell:sub(-2, -1) then
+        quote_function = posix_quote_argument
+      end
+    end
+  end
+
+  return quote_function(str)
 end
 
 return escapeshellarg
+
