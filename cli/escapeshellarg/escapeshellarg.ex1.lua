1b7d9a88409503b2d5491f5bfb62d4977367b2b1
Alexander Schulz <aiqpost@yahoo.com>
2018-05-06 23:22:01 +0200

    Test removes created temp files

--- cli/escapeshellarg/escapeshellarg.ex1.lua
+++ cli/escapeshellarg/escapeshellarg.ex1.lua
@@ -1,23 +1,22 @@
-
-local escapeshellarg = require( "escapeshellarg" )
 local t = require( "taptest" )
+local escapeshellarg = require( "escapeshellarg" )
 
 local function shcompare( got, expected )
-  local w = require( "writefile" )
-  local r = require( "readfile" )
-  w( 'tmp.lua', [[
-    for i=1,#arg do
-      require'writefile'('tmp'..i..'.txt',arg[i])
-    end
-  ]] )
-  local i, luacmd = 0, 'lua'
-  while arg[i] do luacmd = arg[i] ; i = i -1 end
-  os.execute ( luacmd .. [[ tmp.lua ]] .. got .. ' x ' .. got .. ' ' )
-  local g1 = r( 'tmp1.txt' ) or ''
-  local g2 = r( 'tmp2.txt' ) or ''
-  local g3 = r( 'tmp3.txt' ) or ''
-  local ok = g2 == 'x' and g1 == g3 and g3 == expected
-  return ok, not ok and 'Read back [' .. g1 .. '] [' .. g2 .. '] [' .. g3 .. ']'
+   local w = require( "writefile" )
+   local r = require( "readfile" )
+   w( "escape_tmp.lua", [[
+      for i=1,#arg do
+         require'writefile'('escape_tmp'..i..'.txt',arg[i])
+      end
+   ]] )
+   local i, luacmd = 0, "lua"
+   while arg[ i ] do luacmd = arg[ i ] ; i = i -1 end
+   os.execute ( luacmd..[[ escape_tmp.lua ]]..got.." x "..got.." " )
+   local g1 = r( "escape_tmp1.txt" ) or ""
+   local g2 = r( "escape_tmp2.txt" ) or ""
+   local g3 = r( "escape_tmp3.txt" ) or ""
+   local ok = g2 == "x" and g1 == g3 and g3 == expected
+   return ok, not ok and "Read back ["..g1.."] ["..g2.."] ["..g3.."]"
 end
 
 t( escapeshellarg( [[Hello's world]] ),     [[Hello's world]],    shcompare )
@@ -30,6 +29,11 @@ t( escapeshellarg( [[special %PATH%]] ),    [[special %PATH%]],   shcompare )
 t( escapeshellarg( [[redirect>o.txt]] ),    [[redirect>o.txt]],   shcompare )
 t( escapeshellarg( [[redirect<i.txt]] ),    [[redirect<i.txt]],   shcompare )
 t( escapeshellarg( [[pipe|extcmd]] ),       [[pipe|extcmd]],      shcompare )
+
+os.remove( "escape_tmp.lua" )
+os.remove( "escape_tmp1.txt" )
+os.remove( "escape_tmp2.txt" )
+os.remove( "escape_tmp3.txt" )
 
 t()
 
